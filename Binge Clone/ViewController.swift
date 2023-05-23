6e924f7303554b52d28adddad25fb647230311b1
geek-Shayan <shayanulhaqsadi@gmail.com>
2023-05-23 10:49:55 +0600

    Initial Commit

---
+++ Binge Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Binge Clone
+//
+//  Created by MD. SHAYANUL HAQ SADI on 23/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
